aaa764111634f9f30c89a11ba8a5c2c4412c0f37
Jhanet11 <jhanetveizagavallejos@gmail.com>
2026-05-15 10:39:25 -0400

    se agrego main.py

--- main.py
+++ main.py
@@ -1,6 +1,121 @@
-from game.components.game import Game
+import pygame
+from game.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, DEFAULT_TYPE
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.image.load("game/assets/Bullet/bullet_1.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (10, 30))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.bottom = y
+        self.speed = 10
+
+    def update(self):
+        self.rect.y -= self.speed
+        if self.rect.bottom < 0:
+            self.kill()
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+
+class Spaceship(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("game/assets/Spaceship/spaceship.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (80, 80))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = SCREEN_WIDTH // 2
+        self.rect.bottom = SCREEN_HEIGHT - 20
+        self.speed = 8
+        self.bullets = pygame.sprite.Group()
+        self.can_shoot = True
+
+    def update(self):
+        self.handle_input()
+        self.clamp()
+        self.bullets.update()
+
+    def handle_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.rect.x -= self.speed
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.rect.x += self.speed
+
+        click = pygame.mouse.get_pressed()
+        if click[0] and self.can_shoot:
+            self.shoot()
+            self.can_shoot = False
+        if not click[0]:
+            self.can_shoot = True
+
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        self.bullets.add(bullet)
+
+    def clamp(self):
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+        self.bullets.draw(screen)
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption(TITLE)
+        pygame.display.set_icon(ICON)
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.playing = False
+        self.game_speed = 10
+        self.x_pos_bg = 0
+        self.y_pos_bg = 0
+        self.spaceship = Spaceship()
+
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+        pygame.display.quit()
+        pygame.quit()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+
+    def update(self):
+        self.spaceship.update()
+
+    def draw(self):
+        self.clock.tick(FPS)
+        self.screen.fill((255, 255, 255))
+        self.draw_background()
+        self.spaceship.draw(self.screen)
+        pygame.display.update()
+        pygame.display.flip()
+
+    def draw_background(self):
+        image = pygame.transform.scale(BG, (SCREEN_WIDTH, SCREEN_HEIGHT))
+        image_height = image.get_height()
+        self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg))
+        self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg - image_height))
+        if self.y_pos_bg >= SCREEN_HEIGHT:
+            self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg - image_height))
+            self.y_pos_bg = 0
+        self.y_pos_bg += self.game_speed
+
 
 if __name__ == "__main__":
     game = Game()
     game.run()
-    
