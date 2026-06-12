93b1281eb288a80fbaa3ad6c3000ff2937cca352
Adalid32Lopez <adaliylopz@gmail.com>
2026-06-12 11:16:49 -0400

    se creo un script simple

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "¡Hola! Estoy ejecutando tu primer script."
+echo "La fecha y hora actual es:" | date
+
+ls
+
+echo "Creando una carpeta llamada 'Practica_Script'..."
+mkdir Practica_Script
+
+echo "Proceso terminado con éxito."
